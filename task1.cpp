d94021239bd42f8ba825a949eb563baeea2d6ab2
vijay351910 <71546011+vijay351910@users.noreply.github.com>
2020-09-21 16:15:14 +0530

    Add files via upload

--- task1.cpp
+++ task1.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int main()
-{int num1=10,num2=20;
- cout << num1 << "+" << num2 <<'=' << num1+num2 << endl;
- cout << num1 << "-" << num2 <<'='<< num1-num2 << endl;
- cout << num1 << "*" << num2 <<'='<< num1*num2 << endl;
- cout << num1 << "/" << num2 <<'='<< num1/num2 << endl;
- cout << num1 << "%" << num2 <<'='<< num1%num2 << endl;
-
-
+{int num1=10,num2=20,add,sub,mul,quo,rem;
+printf("enter first number:12");
+scanf("%d",&num1);
+printf("enter second number:2");
+scanf("%d,&num2");
+printf("sum of %d and %d is %d",num1,num2,num1+num2);
+printf("difference of %d and %d is %d",num1,num2,num1-num2);
+printf("product of %d and %d is %d",num1,num2,num1*num2);
+printf("on dividing 10 by 20 quotient is %d and %d is %d",num1,num2,num1/num2);
+printf("on dividing 20 by 10 quotient is %d and %d is %d",num1,num2,num2/num1);
 return 0;
-
 }
